492195ab2cbd19b9795065bb041b352a9e1bd959
volkov97 <volkov97@yandex-team.ru>
2019-02-15 00:34:39 +0300

    tests for getTimeParts

---
+++ src/lib/helpers/getTimeParts.test.ts
@@ -0,0 +1,34 @@
+import getTimeParts from './getTimeParts';
+
+interface TimePartsType {
+  ms: number;
+  s: number;
+  m: number;
+  h: number;
+  d: number;
+}
+
+describe('#getTimeParts', () => {
+  [
+    [30, 'ms', { ms: 30 }],
+    [1030, 'ms', { ms: 1030 }],
+    [1030, 's', { s: 1, ms: 30 }],
+    [61030, 's', { s: 61, ms: 30 }],
+    [61030, 'm', { m: 1, s: 1, ms: 30 }],
+    [3601030, 'm', { m: 60, s: 1, ms: 30 }],
+    [3661030, 'h', { h: 1, m: 1, s: 1, ms: 30 }],
+    [86461030, 'h', { h: 24, m: 1, s: 1, ms: 30 }],
+    [90061030, 'd', { d: 1, h: 1, m: 1, s: 1, ms: 30 }],
+  ].forEach(([time, lastUnit, result]) => {
+    it(`should return right result for time = ${time} and lastUnit = ${lastUnit}`, () => {
+      expect(getTimeParts(time, lastUnit)).toStrictEqual({
+        ms: 0,
+        s: 0,
+        m: 0,
+        h: 0,
+        d: 0,
+        ...(result as TimePartsType),
+      });
+    });
+  });
+});
