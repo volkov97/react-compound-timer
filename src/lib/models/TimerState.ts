eb74ccfbc5b4e3a749b6d3f5809b50fe63bcbcfc
volkov97 <volkov97@yandex-team.ru>
2019-02-15 00:05:32 +0300

    rewrite TimerState to ts

---
+++ src/lib/models/TimerState.ts
@@ -0,0 +1,84 @@
+export const INITED = 'INITED';
+export const PLAYING = 'PLAYING';
+export const PAUSED = 'PAUSED';
+export const STOPPED = 'STOPPED';
+
+type TimerStates = 'INITED' | 'PLAYING' | 'PAUSED' | 'STOPPED';
+
+export default class TimerState {
+  private onChange: () => any;
+  private state: TimerStates = INITED;
+
+  constructor(onChangeStatus = (obj: { timerState: TimerStates }) => {}) {
+    this.onChange = () => onChangeStatus({ timerState: this.state });
+    this.state = INITED;
+  }
+
+  public getState() {
+    return this.state;
+  }
+
+  public setInited() {
+    if (this.state === INITED) {
+      return false;
+    }
+
+    this.state = INITED;
+
+    this.onChange();
+
+    return true;
+  }
+
+  public isInited() {
+    return this.state === INITED;
+  }
+
+  public setPlaying() {
+    if (this.state === PLAYING) {
+      return false;
+    }
+
+    this.state = PLAYING;
+
+    this.onChange();
+
+    return true;
+  }
+
+  public isPlaying() {
+    return this.state === PLAYING;
+  }
+
+  public setPaused() {
+    if (this.state !== PLAYING) {
+      return false;
+    }
+
+    this.state = PAUSED;
+
+    this.onChange();
+
+    return true;
+  }
+
+  public isPaused() {
+    return this.state === PAUSED;
+  }
+
+  public setStopped() {
+    if (this.state === INITED) {
+      return false;
+    }
+
+    this.state = STOPPED;
+
+    this.onChange();
+
+    return true;
+  }
+
+  public isStopped() {
+    return this.state === STOPPED;
+  }
+}
