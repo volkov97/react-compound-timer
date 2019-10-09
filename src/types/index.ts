e1e64e4ffb00aefa3861b73cebec6084cbb84094
volkov97 <Volkov.german.1997@gmail.com>
2019-10-09 11:37:57 +0300

    add timer controls type

--- src/types/index.ts
+++ src/types/index.ts
@@ -20,3 +20,18 @@ export type TimerStateValues = 'INITED' | 'PLAYING' | 'PAUSED' | 'STOPPED';
 export type Direction = 'forward' | 'backward';
 
 export type TimerValue = TimeParts & { state: TimerStateValues };
+
+export interface TimerControls {
+  start: () => void;
+  stop: () => void;
+  pause: () => void;
+  reset: () => void;
+  resume: () => void;
+  setTime: (time: number) => void;
+  getTime: () => number;
+  getTimerState: () => TimerStateValues;
+  setDirection: (direction: Direction) => void;
+  setLastUnit: (lastUnit: Unit) => void;
+  setTimeToUpdate: (interval: number) => void;
+  setCheckpoints: (checkpoints: Checkpoint[]) => void;
+}
