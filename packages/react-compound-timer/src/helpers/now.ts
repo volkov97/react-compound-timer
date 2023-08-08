03056ad1b6a6265d3bf1302b5bb022d8c75df8bf
German Volkov <volkov.german.1997@gmail.com>
2023-08-09 00:28:57 +0200

    feat: use Date.now() to detect current ts

--- packages/react-compound-timer/src/helpers/now.ts
+++ packages/react-compound-timer/src/helpers/now.ts
@@ -1,7 +1,11 @@
 export function now(): number {
-  if (typeof window === 'undefined' || !('performance' in window)) {
-    return Date.now();
-  }
+  // if (typeof window === 'undefined' || !('performance' in window)) {
+  //   return Date.now();
+  // }
+  // return performance.now();
 
-  return performance.now();
+  // we do not use performance.now() anymore, read links below for more info
+  // https://medium.com/@mihai/performance-now-sleep-16a5b774560c
+  // https://persistent.info/web-experiments/performance-now-sleep/
+  return Date.now();
 }
